68b49fa8016089ccefda5f72a07c34fb909b39a2
Kurt <moffkr91@gmail.com>
2018-07-23 14:07:34 -0700

    switched to HashRouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+import { HashRouter as Router, Route, Link } from 'react-router-dom'
 import { LandingPage, HomePage } from './components/index'
 import './App.css';
 
