d06ed7ae8e94b3861cb50f71a2b7d5131611dd1a
Kurt <moffkr91@gmail.com>
2018-07-23 13:28:39 -0700

    qs

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -1,9 +1,19 @@
 const cookie = require('./cookie')
+const crypto = require('crypto')
+
+const nonce = () => {
+        let text = ''
+        let selection = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+	    for (i=0; i<20; i++) {
+                text += selection.charAt(Math.floor(Math.random()*selection.length))
+            }
+        return text
+    }
 
 module.exports = (app) => {
 
 	app.get('/', (req, res) => {
-        res.cookie('sessid' , cookie).send('Cookie set');
+        res.cookie('sessid' , crypto.createHash('sha256').update(`${nonce()+Date.now()}`).digest('hex')).send('Cookie set');
         //res.sendFile(path.join( __dirname, 'build'));
         //res.send('Hello World!')
       });
