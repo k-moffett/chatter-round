f368b448a44d38d67f2bf6e9a422c658912f03b7
Kurt <moffkr91@gmail.com>
2018-07-31 09:58:59 -0700

    qs

--- backend/routes.js
+++ backend/routes.js
@@ -12,9 +12,7 @@ const nonce = () => {
 module.exports = (app) => {
 
 	app.get('/', (req, res) => {
-        res.cookie('sessid' , crypto.createHash('sha256').update(`${nonce()+Date.now()}`).digest('hex'), {
-            httpOnly : false
-        }).sendFile(path.join( __dirname, 'build'));
+        res.cookie('sessid' , crypto.createHash('sha256').update(`${nonce()+Date.now()}`).digest('hex')).sendFile(path.join( __dirname, 'build'));
       });
       
 }
