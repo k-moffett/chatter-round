4eaeb575733da277214171d3442f8b6f4ecdd1a4
Kurt <moffkr91@gmail.com>
2018-07-31 10:14:25 -0700

    'qs'

--- backend/routes.js
+++ backend/routes.js
@@ -1,5 +1,4 @@
-const crypto = require('crypto');
-const path = require('path');
+const crypto = require('crypto')
 
 const nonce = () => {
         let text = ''
@@ -13,8 +12,7 @@ const nonce = () => {
 module.exports = (app) => {
 
 	app.get('/', (req, res) => {
-        res.sendFile(path.join( __dirname, '../build'));
-        //res.cookie('sessid' , crypto.createHash('sha256').update(`${nonce()+Date.now()}`).digest('hex')).sendFile(path.join( __dirname, 'build'));
+        res.cookie('sessid' , crypto.createHash('sha256').update(`${nonce()+Date.now()}`).digest('hex')).sendFile(path.join( __dirname, 'build'));
       });
       
 }
