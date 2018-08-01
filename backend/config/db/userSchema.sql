773b88aa841254cd9bb7d76e7268b6ed3b8fe639
Kurt <moffkr91@gmail.com>
2018-08-01 14:24:20 -0700

    added user schema

---
+++ backend/config/db/userSchema.sql
@@ -0,0 +1,11 @@
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE users (
+    id int AUTO_INCREMENT, 
+    username VARCHAR(50), 
+    email VARCHAR(80),
+    dateOfBirth VARCHAR(12),
+    password VARCHAR(16), 
+    sessid VARCHAR(30), 
+    PRIMARY KEY (id)
+);
