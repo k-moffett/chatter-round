741aa86e1d1c7a5dae8fd8876f33c9c89ed5d9a5
Kurt <moffkr91@gmail.com>
2018-08-03 11:05:23 -0700

    'qs'

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -3,7 +3,7 @@ const userModel = require('../models/userModel')
 const userController = {
 
     userSignUp(userInfo, /*sessid*/) {
-        return userModel.doesEmailExist(userInfo, sessid)
+        return userModel.doesEmailExist(userInfo, /*sessid*/)
     },
 
     userLogin() {
