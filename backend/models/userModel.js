f2e91b22bab919ccbb24f73c5a7801d7e182e07c
Kurt <moffkr91@gmail.com>
2018-08-03 11:06:26 -0700

    'qs'

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -2,6 +2,15 @@ const crypto = require('crypto');
 const connection = require('../config/db/connection');
 connection.connect();
 
+const nonce = () => {
+    let text = '';
+    let selection = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    for (i=0; i<20; i++) {
+            text += selection.charAt(Math.floor(Math.random()*selection.length));
+        };
+    return text;
+}
+
 const userModel = {
 
     doesEmailExist(userInfo, /*sessid*/) {
@@ -25,7 +34,7 @@ const userModel = {
 
     createUser(userInfo, /*sessid*/) {
         let sessid = crypto.createHash('sha256').update(`${nonce()+Date.now()}`).digest('hex')
-        
+
             return new Promise((resolve, reject) => {
 
                 connection.query(`INSERT INTO users (username, email, dateOfBirth, password, sessid) VALUES (${connection.escape(userInfo.userName)}, ${connection.escape(userInfo.email)}, ${connection.escape(userInfo.dateOfBirth)}, ${connection.escape(userInfo.password)}, ${connection.escape(sessid)});`, function (error, results, fields) {
