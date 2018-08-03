6b30eaf1743a75ac74c1ed56d35ac864098886db
Kurt <moffkr91@gmail.com>
2018-08-03 11:27:57 -0700

    'qs'

--- backend/routes.js
+++ backend/routes.js
@@ -11,7 +11,7 @@ module.exports = (app) => {
         userController.userSignUp(userInfo, /*sessid*/)
         .then((response) => {
             console.log( '/signup RESPONSE: ', response)
-            console.log(respnse.sessid)
+            console.log(response.sessid)
             res.cookie('sessid', response.sessid)
         })
         .catch((error) => {console.log(error)});
