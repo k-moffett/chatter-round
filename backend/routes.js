c4e961a45e4654035c79ff3bbc1ed7394d97825c
Kurt <moffkr91@gmail.com>
2018-08-05 17:27:03 -0700

    'qs'

--- backend/routes.js
+++ backend/routes.js
@@ -12,7 +12,7 @@ const nonce = () => {
 
 module.exports = (app) => {
 
-	app.get('/*', (req, res) => {
+	app.get('*', (req, res) => {
         res.sendFile(path.join( __dirname, 'build'));
       });
 
