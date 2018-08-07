16e070b7e57adc52568b042884d9e163a59c0f3e
Kurt <moffkr91@gmail.com>
2018-08-07 10:54:39 -0700

    'qs'

--- src/components/homePage/googleMap.js
+++ src/components/homePage/googleMap.js
@@ -5,9 +5,28 @@ import { Container, Row, Col, Button } from 'reactstrap';
 export default class Map extends Component{
     constructor(props) {
         super(props)
+        this.retrieveGoogleAPI = this.retrieveGoogleAPI.bind(this)
     }
 
     componentWillMount() {
+        this.retrieveGoogleAPI()
+    }
+
+    retrieveGoogleAPI() {
+        fetch('`https://maps.googleapis.com/maps/api/js?key=${process.env.REACT_APP_GOOGLE_KEY}&callback=initMap`', {
+            method: 'GET',
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json',
+            },
+        })
+            .then((response) => response.json())
+            .then((responseJson) => {
+                console.log(responseJson)
+            })
+            .catch((error) => {
+                console.error(error);
+            });
     }
 
     render() {
