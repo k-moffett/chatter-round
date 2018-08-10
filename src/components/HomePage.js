1264978803488e64de98a1f5018e90182a60e13f
kmoffett <moffkr91@gmail.com>
2018-08-10 11:49:31 -0700

    qs

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -19,7 +19,6 @@ export default class HomePage extends Component {
         this.getCoords = this.getCoords.bind(this)
         this.convertPosition = this.convertPosition.bind(this)
         this.getChats = this.getChats.bind(this)
-        this.setChats = this.setChats.bind(this)
     }
 
     componentWillMount() {
