17007e051cd79bf00d97d67deb637289a32c53ef
kmoffett <moffkr91@gmail.com>
2018-08-13 14:05:14 -0700

    qs

--- src/components/homepage/chat.js
+++ src/components/homepage/chat.js
@@ -4,18 +4,6 @@ import { Container, Row, Col, Button, Form, FormGroup, Input, Label } from 'reac
 const moment = require('moment');
 const firebase = require('firebase')
 
-const config = {
-    apiKey: process.env.REACT_APP_API_KEY,
-    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-    databaseURL: process.env.REACT_APP_DATABASE_URL,
-    projectId: process.env.REACT_APP_PROJECT_ID,
-    storageBucket: "",
-    messagingSenderId: process.env.REACT_APP_MSG_SENDER_ID
-  };
-
-firebase.initializeApp(config);
-
-
 export default class Chat extends Component {
     constructor(props) {
         super(props)
